083e3f994a228b1c9509438c5788497553246001
iprahimelstuhy <iprahimelstuhy@gmail.com>
2024-04-30 04:33:50 +0200

    add checkpass

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 #include "admin.h"
 #include "user.h"
 #include "list.h"
-
 int TotalStudents=5;
 
 
@@ -16,15 +15,24 @@ struct student school[MAXSTUDENTS]= {{"1005","omar","123456","3.5",20,2},
     {"1005","layla","741252","3.6",23,5}
 };
 
+int main() {
+    int mode;
 
+    printf("---->Welcome to Student Record System<----\n");
+    printf("[1]. Admin Mode\n");
+    printf("[2]. User Mode\n");
+    scanf("%d", &mode);
 
-
-
-int main()
-{
-    mode();
+    if (mode == 1) {
+       passcheck();
+    } else if (mode == 2) {
+     studentMode();
+         } else {
+        printf("Invalid choice. Exiting program.\n");
+    }
 
     return 0;
 }
+
 
 
--- user.h
+++ user.h
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include "list.h"
 void viewRecord(struct student *arr, int index);
-void studentMode(struct student *arr, int TotalStudents);
+void studentMode();
 void editName(struct student *arr, int index);
 void editPassword(struct student *arr, int index);
 void viewRecord(struct student *arr, int index);
